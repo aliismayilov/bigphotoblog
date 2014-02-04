c8698ee10a4c5c69c0010946a878aa168f66eb9d
Ali Ismayilov <ali@ismailov.info>
2014-02-04 17:42:49 -0200

    initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bigphotoblog::Application.config.secret_key_base = '1d0ebedab1560f288d88a66ced3a4501b4ba7813104ce2595cd9158b41bb58332d7d9e9f04b23ae55bfd4cd46aec7c5189e692224119cfda7428dedcbc2f8ba3'
